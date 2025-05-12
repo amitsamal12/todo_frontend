de0c53982cd9b3ec8cc96e426f9ec13d5ab64f2e
Amit-SDE <amitsamal1998@gmail.com>
2025-05-12 20:50:50 +0530

    Registartion

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Login from './components/Login'
+import Registration from './components/Registration'
 
 function App() {
   
 
   return (
     <div className='main-container'>
-      <Login />
+      {/* <Login /> */}
+      <Registration/>
     </div>
   )
 }
